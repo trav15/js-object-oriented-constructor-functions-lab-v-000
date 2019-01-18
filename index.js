60ece20dc4af08d0699d8f39025956b7c8e51d86
Travis T <travistokuyama@gmail.com>
2019-01-18 02:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function Scooter(year, color, model){
   this.model = model;
 }
 
+function Driver(name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation(address, city){
+  this.address = address;
+  this.city = city;
+}
